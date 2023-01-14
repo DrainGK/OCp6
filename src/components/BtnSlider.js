3aafffcdaab77ca258204c1204f876c10004cff7
Guillaume Lopez <guillaumelopezgk@hotmail.com>
2023-01-14 12:18:01 +0900

    P6 v5

---
+++ src/components/BtnSlider.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import rightArrow from "../../src/assets/img/arrow-right.svg";
+// import leftArrow from "../../src/assets/img/arrow_left.svg";
+
+// const BtnSlider = ({ direction, moveSlide }) => {
+//   return (
+//     <button
+//       onClick={moveSlide}
+//       className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+//     >
+//       <img src={direction === next ? rightArrow : leftArrow} />
+//     </button>
+//   );
+// };
+
+// export default BtnSlider;
